e0eb8f23ac675b577104fba2addeeae860c1d952
Lorenzo Longhitano <Lorenzo.Longhitano@dedagroup.it>
2023-12-25 11:36:36 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+
+//definire le routes supportate dall'applicazione
+//ovvero impostare le corrispondenze tra i path dell'URL e dei componenti
+createBrowserRouter([
+  {},{}
+]);
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
